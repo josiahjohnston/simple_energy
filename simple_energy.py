632f73d1c7ca5b9e62216eaebac219f587648f91
josiah.johnston <josiah.johnston@gmail.com>
2016-04-11 11:20:54 -0700

    Added timepoints

--- simple_energy.py
+++ simple_energy.py
@@ -11,27 +11,36 @@ from pyomo.environ import *
 # are added next.
 model = AbstractModel()
 
+# A set of timepoints to dispatch across. Sets are written in ALL_CAPS_WITH_UNDERSCORES
+# These will be indexes to some parameters, variables and constraints, so I'll
+# define them at the top. 
+# Their labels can be anything I want: a word, timestamp or whatever.
+model.TIMEPOINTS = Set(initialize=['peak', 'mean'])
+
 # Define parameters along with default values so we don't have to use a 
 # separate data file.
-model.load_mwh = Param(default=17520)
-model.dispatch_cost = Param(default=1, doc="dispatch cost in $/MWh")
+model.timepoint_duration_hr = Param(model.TIMEPOINTS, default={'peak': 10, 'mean':8750})
+model.load_mw = Param(model.TIMEPOINTS, default={'peak': 4, 'mean':2})
+model.dispatch_cost = Param(model.TIMEPOINTS, default=1, doc="dispatch cost in $/MWh")
 
 # Decision variables
-model.DispatchMWh = Var()
+model.DispatchMW = Var(model.TIMEPOINTS)
 
 # The objective is written in two steps: 
 # 1: A python function that takes a model as an argument at run time, and returns
 # a mathematical formula using components from the model.
 # 2: Attach the python function to the model as the objective.
 def Dispatch_Cost_rule(mod):
-    return mod.dispatch_cost * mod.DispatchMWh
+    return sum(
+        mod.dispatch_cost[t] * mod.DispatchMW[t] * mod.timepoint_duration_hr[t]
+        for t in mod.TIMEPOINTS)
 model.DispatchCost = Objective(rule=Dispatch_Cost_rule)
 
 # The constraint is also written in two steps, and needs to return
 # either an equality or inequality relationship.
-def Conservation_Of_Energy_rule(mod):
-    return mod.load_mwh == mod.DispatchMWh
-model.Conservation_Of_Energy = Constraint(rule=Conservation_Of_Energy_rule)
+def Conservation_Of_Energy_rule(mod, t):
+    return mod.load_mw[t] == mod.DispatchMW[t]
+model.Conservation_Of_Energy = Constraint(model.TIMEPOINTS, rule=Conservation_Of_Energy_rule)
 
 
 # Some boilerplate code to execute this model with `python simple_energy.py`
