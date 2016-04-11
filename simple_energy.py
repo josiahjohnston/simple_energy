cabe4f1fa0f1e874e9af9df87aac4b6a278b822f
josiah.johnston <josiah.johnston@gmail.com>
2016-04-11 10:47:32 -0700

    Now that this is a python script, we could rename our model to whatever we want. The downside is that we won't be able to use the pyomo or pysp command line tools anymore.

--- simple_energy.py
+++ simple_energy.py
@@ -9,15 +9,15 @@ from pyomo.environ import *
 # Start by creating a model object. Store it in a variable named "model" if
 # you want to work with the pyomo or pysp command-line tools. Model components
 # are added next.
-model = AbstractModel()
+energy_model = AbstractModel()
 
 # Define parameters along with default values so we don't have to use a 
 # separate data file.
-model.load_mwh = Param(default=2)
-model.dispatch_cost = Param(default=1, doc="dispatch cost in $/MWh")
+energy_model.load_mwh = Param(default=2)
+energy_model.dispatch_cost = Param(default=1, doc="dispatch cost in $/MWh")
 
 # Decision variables
-model.DispatchMWh = Var()
+energy_model.DispatchMWh = Var()
 
 # The objective is written in two steps: 
 # 1: A python function that takes a model as an argument at run time, and returns
@@ -25,13 +25,13 @@ model.DispatchMWh = Var()
 # 2: Attach the python function to the model as the objective.
 def Dispatch_Cost_rule(mod):
     return mod.dispatch_cost * mod.DispatchMWh
-model.DispatchCost = Objective(rule=Dispatch_Cost_rule)
+energy_model.DispatchCost = Objective(rule=Dispatch_Cost_rule)
 
 # The constraint is also written in two steps, and needs to return
 # either an equality or inequality relationship.
 def Conservation_Of_Energy_rule(mod):
     return mod.load_mwh == mod.DispatchMWh
-model.Conservation_Of_Energy = Constraint(rule=Conservation_Of_Energy_rule)
+energy_model.Conservation_Of_Energy = Constraint(rule=Conservation_Of_Energy_rule)
 
 
 # Some boilerplate code to execute this model with `python simple_energy.py`
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     from pyomo.opt import SolverFactory
     import pyomo.environ
     opt = SolverFactory("glpk")
-    model_instance = model.create_instance()
+    model_instance = energy_model.create_instance()
     results = opt.solve(model_instance)
     model_instance.pprint()
     results.write()    
